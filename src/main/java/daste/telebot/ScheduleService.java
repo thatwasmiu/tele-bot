06950d8f233b66123e0b65e7dfdd2895192b4eee
Tien Dat <the0leon006@gmail.com>
2023-09-18 08:51:58 +0700

    Working commit

--- src/main/java/daste/telebot/ScheduleService.java
+++ src/main/java/daste/telebot/ScheduleService.java
@@ -15,19 +15,20 @@ import java.util.Arrays;
 @Configuration
 @EnableScheduling
 public class ScheduleService {
-    @Scheduled(cron = "0 */30 * ? * *")
+    @Scheduled(cron = "0 */1 * ? * *")
 //    @Scheduled(cron = "0 * * * *")
     public void scheduleTaskUsingCronExpression() {
         BotAction action = BotFactory.getBotAction();
         for (int i = 0; i < 5; i++) {
 
             String text = "Bao cao jsfjskfdjasdklfjasdkfljasdofajsdfkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
+            ClassLoader classLoader = getClass().getClassLoader();
             File image = null;
-            if (i == 0) image = new File("D:\\tele-bot\\src\\main\\resources\\templates\\1.jpeg");
-            if (i == 1) image = new File("D:\\tele-bot\\src\\main\\resources\\templates\\2.png");
-            if (i == 2) image = new File("D:\\tele-bot\\src\\main\\resources\\templates\\3.png");
-            if (i == 3) image = new File("D:\\tele-bot\\src\\main\\resources\\templates\\4.jpeg");
-            if (i == 4) image = new File("D:\\tele-bot\\src\\main\\resources\\templates\\finish.png");
+            if (i == 0) image = new File(classLoader.getResource("templates").getPath() + "/1.jpeg");
+            if (i == 1) image = new File(classLoader.getResource("templates").getPath() +"/2.png");
+            if (i == 2) image = new File(classLoader.getResource("templates").getPath() + "/3.png");
+            if (i == 3) image = new File(classLoader.getResource("templates").getPath() + "/4.jpeg");
+            if (i == 4) image = new File(classLoader.getResource("templates").getPath() + "/finish.png");
             action.sendScheduledReport(Arrays.asList("-1001905190012"), text, image);
         }
     }
